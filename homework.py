3ed16ea2356b5979ad80e6b2af5d9d90f2a9711d
MING <chming1016>
2016-10-19 13:26:12 +0800

    add homework.py homework2.py

---
+++ homework.py
@@ -0,0 +1,14 @@
+﻿# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+K = 9100
+Premium_Call = 179
+Premium_Put = 185
+Interval = 500
+ST = np.arange(K - Interval, K + Interval)  #模擬未來股價的價位
+Payoff_LongCall = np.maximum(ST - K, 0) - Premium_Call
+Payoff_ShortCall = -Payoff_LongCall
+plt.plot(ST, Payoff_LongCall)
+plt.plot(ST, Payoff_ShortCall)
+plt.show()
+
