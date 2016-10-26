1eeab32a2f781d58fc42d74567beec43c5853b11
MING <chming1016>
2016-10-26 13:21:30 +0800

    update hw2

--- homework2.py
+++ homework2.py
@@ -2,13 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 K = 9100
-Premium_Call = 185
-Premium_Put = 179
+Premium_Call = 179
+Premium_Put = 185
 Interval = 500
 ST = np.arange(K - Interval, K + Interval)  #模擬未來股價的價位
-Payoff_LongCall = np.maximum(ST - K, 0) - Premium_Call
-Payoff_ShortCall = -Payoff_LongCall
-plt.plot(ST, Payoff_LongCall)
-plt.plot(ST, Payoff_ShortCall)
+Payoff_LongPut = np.maximum(K - ST, 0) - Premium_Put
+Payoff_ShortPut = -Payoff_LongPut
+plt.plot(ST, Payoff_LongPut)
+plt.plot(ST, Payoff_ShortPut)
 plt.show()
 
