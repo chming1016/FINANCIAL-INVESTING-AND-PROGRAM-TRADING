3d8db94ae082ce332994ceede719e73f9837af8d
MING <chming1016>
2017-01-02 17:15:44 +0800

    updated

--- Project.py
+++ Project.py
@@ -12,10 +12,8 @@ stock_data.sort_values(by='Date', inplace=False)
 for i in range(len(stock_list)):
     stock_data['MA20_' + str(stock_list[i])] = pd.rolling_mean(stock_data[str(stock_list[i])], 20)
 stock_data.sort_values('Date', ascending=True, inplace=True)
-stock_data.to_csv('D://Log/ma_data.csv', index=False)
-ma_data = pd.read_csv('D://Log/ma_data.csv', parse_dates=[0])
 for i in range(len(stock_list)):
-    for j in range(len(ma_data.index)):
-        if(ma_data[str(stock_list[i])][j] > ma_data['MA20_'+str(stock_list[i])][j]):
-            ma_data = ma_data.set_value(j, 'new_'+str(stock_list[i]), 'V')
-ma_data.to_csv('D://Log/ma_data.csv', index=False)
+    for j in range(len(stock_data.index)):
+        if(stock_data[str(stock_list[i])][j] > stock_data['MA20_'+str(stock_list[i])][j]):
+            stock_data = stock_data.set_value(j, 'new_'+str(stock_list[i]), 'V')
+stock_data.to_csv('D://Log/output.csv', index=False)
